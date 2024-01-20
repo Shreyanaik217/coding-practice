e52b4e6eaad01c64a69f14280d19af3b017217c5
Shreya S Naik <131464948+Shreyanaik217@users.noreply.github.com>
2024-01-20 10:54:46 +0530

    Update NumberPowerOf2-BitManipulation.py

--- NumberPowerOf2-BitManipulation.py
+++ NumberPowerOf2-BitManipulation.py
@@ -1,3 +1,5 @@
+#The is_power_of_two function checks whether a given number n is a power of two. It utilizes bitwise operations to efficiently perform this check.
+
 def is_power_of_two(n):
     return n > 0 and (n & (n - 1)) == 0
 
