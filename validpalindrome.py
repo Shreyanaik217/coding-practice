737926e51d9959dc2c81bc13c4015679e632d9d6
Shreya S Naik <131464948+Shreyanaik217@users.noreply.github.com>
2024-01-20 11:04:09 +0530

    Update validpalindrome.py

--- validpalindrome.py
+++ validpalindrome.py
@@ -3,3 +3,12 @@ class Solution(object):
     def isPalindrome(self, s):
         s = [c.lower() for c in s if c.isalnum()]
         return all (s[i] == s[~i] for i in range(len(s)//2))
+
+************************************************************************************************************************
+#simplified
+
+sol = Solution()
+print(sol.isPalindrome("A man, a plan, a canal, Panama"))  # Output: True
+print(sol.isPalindrome("race a car"))  # Output: False
+
+#inbuild functions
